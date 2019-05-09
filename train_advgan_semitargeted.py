190bc16d4cb19a02a08b68bce6949e67ba91330a
Weijia <zhangweijia101@gmail.com>
2019-05-09 19:39:46 +0100

    added semi-targeted attack

---
+++ train_advgan_semitargeted.py
@@ -0,0 +1,152 @@
+# Note that for semi-targeted attack, there is no need to re-train the target model
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+import torch.nn.functional as F
+from torch.optim.lr_scheduler import StepLR
+
+import target_models
+from generators import Generator_MNIST as Generator
+from discriminators import Discriminator_MNIST as Discriminator
+from prepare_dataset import load_dataset
+from train_function import train_semitargeted
+from test_function import test_semitargeted
+
+import cv2
+import numpy as np
+import os
+import argparse
+
+def CWLoss(logits, target, num_classes=10, kappa=0):
+    # inputs to the softmax function are called logits.
+    # https://arxiv.org/pdf/1608.04644.pdf
+    target_one_hot = []
+    #print(target.size()) # torch.Size([128])
+    #print(logits.size()) # torch.Size([128,10])
+    #print(target.type()) # torch.cuda.LongTensor
+    #print(logits.type()) # torch.cuda.FloatTensor
+    for x in target.cpu().numpy():
+        temp = torch.eye(num_classes).type(logits.type())[x]
+        #print(np.shape(temp)) # torch.Size([10])
+        #print(temp.type()) # torch.cuda.FloatTensor
+        #print(temp.type()) # torch.cuda.FloatTensor
+        temp = temp.cpu()
+        #print(temp.type()) # torch.FloatTensor
+        temp = temp.numpy()
+        #print(type(temp)) # <class 'numpy.ndarray'>
+        #print(np.shape(temp)) # (10,)
+        target_one_hot.append(temp)        
+    #print(target_one_hot)   
+    
+    # convert list to tensor
+#    target_one_hot = torch.FloatTensor(target_one_hot)
+#    print(target_one_hot.type())# torch.FloatTensor
+#    print(target_one_hot.size()) # torch.Size([128, 10])
+    
+    target_one_hot = torch.FloatTensor(target_one_hot).to(device)
+    #print(target_one_hot.type())# torch.cuda.FloatTensor
+    #print(target_one_hot.size()) # torch.Size([128, 10])
+    #print(logits.type()) # torch.cuda.FloatTensor
+    #print(logits.size()) # torch.Size([128, 10])
+    # workaround here.
+    # subtract large value from target class to find other max value
+    # https://github.com/carlini/nn_robust_attacks/blob/master/l2_attack.py
+    
+    #print(len(target_one_hot)) # 128
+    real = torch.sum(target_one_hot*logits, 1)
+    #temp2 = target_one_hot*logits
+    #print(temp2.type()) # torch.cuda.FloatTensor
+    #print(temp2.size()) # torch.Size([128, 10])
+    #print(real.size()) # torch.Size([128])
+    #temp3 = 1-target_one_hot
+    #print(temp3.size()) # torch.Size([128, 10])
+    
+    other = torch.max((1-target_one_hot)*logits - (target_one_hot*10000), 1)[0]
+    #print(other.size()) # torch.Size([128])
+    kappa = torch.zeros_like(other).fill_(kappa)
+
+    return torch.sum(torch.max(other-real, kappa))
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser(description='Train AdvGAN')
+    parser.add_argument('--model', type=str, default="Model_C", required=False, choices=["Model_A", "Model_B", "Model_C"], help='model name (default: Model_C)')
+    parser.add_argument('--epochs', type=int, default=15, required=False, help='no. of epochs (default: 30)')
+    parser.add_argument('--batch_size', type=int, default=128, required=False, help='batch size (default: 128)')
+    parser.add_argument('--lr', type=float, default=0.001, required=False, help='learning rate (default: 0.001)')
+    parser.add_argument('--num_workers', type=int, default=4, required=False, help='no. of workers (default: 4)')
+    parser.add_argument('--thres', type=float, required=False, default=0.3, help='Perturbation bound')
+    parser.add_argument('--gpu', action='store_true', default=True, help='Use GPU?')
+
+    args = parser.parse_args()
+    lr = args.lr
+    batch_size = args.batch_size
+    num_workers = args.num_workers
+    epochs = args.epochs
+    model_name = args.model
+#    thres = args.thres # thres is hard-coded below, change it
+    gpu = args.gpu
+    dataset_name = 'mnist'
+
+    # alternatively set parameters here
+    thres = 0.3
+    model = 'Model_C'
+    img_path = 'images/0.jpg'
+    is_targeted = True # semi targeted
+  
+    print('Training AdvGAN (Semitargeted)')
+    
+    train_data, test_data, in_channels, num_classes = load_dataset(dataset_name)
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+    D = Discriminator()
+    G = Generator()
+    f = getattr(target_models, model_name)(in_channels, num_classes)
+
+    checkpoint_path = os.path.join('saved', 'target_models', 'semitargeted', 'best_%s_mnist.pth.tar'%(model_name))
+    checkpoint = torch.load(checkpoint_path, map_location='cpu')
+    f.load_state_dict(checkpoint["state_dict"])
+    f.eval()
+
+    if gpu:
+        D.cuda()
+        G.cuda()
+        f.cuda()
+
+    optimizer_G = optim.Adam(G.parameters(), lr=lr)
+    optimizer_D = optim.Adam(D.parameters(), lr=lr)
+
+    scheduler_G = StepLR(optimizer_G, step_size=5, gamma=0.1)
+    scheduler_D = StepLR(optimizer_D, step_size=5, gamma=0.1)
+
+    criterion_adv =  CWLoss # loss for fooling target model
+    criterion_gan = nn.MSELoss() # for gan loss
+    alpha = 1 # gan loss multiplication factor
+    beta = 1 # for hinge loss
+    num_steps = 3 # number of generator updates for 1 discriminator update
+    thres = c = 0.3 # perturbation bound, used in loss_hinge
+
+    device = 'cuda' if gpu else 'cpu'
+
+    for epoch in range(epochs):
+        acc_train = train_semitargeted(G, D, f, thres, criterion_adv, criterion_gan, alpha, beta, train_loader, optimizer_G, optimizer_D, epoch, epochs, device, num_steps, verbose=True)
+        acc_test = test_semitargeted(G, f, thres, test_loader, epoch, epochs, device, verbose=True)
+
+        scheduler_G.step()
+        scheduler_D.step()
+
+        print("     "*20, end="\r")
+        print('Epoch [%d/%d]\t\t\t'%(epoch+1, epochs))
+        print('Train Acc: %.5f'%(acc_train))
+        print('Test Acc: %.5f'%(acc_test))
+        print('\n')
+
+        torch.save({"epoch": epoch+1,
+                    "epochs": epochs,
+                    "thres": thres,
+                    "state_dict": G.state_dict(),
+                    "acc_test": acc_test,
+                    "optimizer": optimizer_G.state_dict()
+                    }, "saved/target_models/semitargeted/generator/%s_%s.pth.tar"%(model_name, 'semitargeted'))
--- train_function.py
+++ train_function.py
@@ -1,5 +1,6 @@
 import torch
 from torch.autograd import Variable
+#from operator import itemgetter
 
 
 def train(G, D, f, target, is_targeted, thres, criterion_adv, criterion_gan, alpha, beta, train_loader, optimizer_G, optimizer_D, epoch, epochs, device, num_steps=3, verbose=True):
@@ -26,12 +27,81 @@ def train(G, D, f, target, is_targeted, thres, criterion_adv, criterion_gan, alp
         # adversarial loss
         if is_targeted:
             y_target = Variable(torch.ones_like(label).fill_(target).to(device))
+            
             loss_adv = criterion_adv(y_pred, y_target, is_targeted)
             acc += torch.sum(torch.max(y_pred, 1)[1] == y_target).item()
         else:
             y_true = Variable(label.to(device))
             loss_adv = criterion_adv(y_pred, y_true, is_targeted)
             acc += torch.sum(torch.max(y_pred, 1)[1] != y_true).item()
+
+        # GAN Generator loss
+        loss_gan = criterion_gan(D(img_fake), valid)
+        # perturbation loss
+        loss_hinge = torch.mean(torch.max(torch.zeros(1, ).type(y_pred.type()), torch.norm(pert.view(pert.size(0), -1), p=2, dim=1) - thres))
+        # total generator loss
+        loss_g = loss_adv + alpha*loss_gan + beta*loss_hinge
+        # alternative loss functions as described in paper
+        #loss_g =  torch.norm(pert.view(pert.size(0), -1), p=2, dim=1) + loss_adv # pert norm + adv loss
+        #loss_g = loss_hinge + loss_adv # pert loss + adv loss
+        
+        loss_g.backward(torch.ones_like(loss_g))
+        optimizer_G.step()
+
+        optimizer_D.zero_grad()
+        if i % num_steps == 0:
+            # Train the Discriminator
+            loss_real = criterion_gan(D(img_real), valid)
+            loss_fake = criterion_gan(D(img_fake.detach()), fake)
+            loss_d = 0.5*loss_real + 0.5*loss_fake
+            loss_d.backward(torch.ones_like(loss_d))
+            optimizer_D.step()
+
+        n += img.size(0)
+
+        #print("Epoch [%d/%d]: [%d/%d], D Loss: %1.4f, G Loss: %3.4f [H %3.4f, A %3.4f], Acc: %.4f"
+        #    %(epoch+1, epochs, i, len(train_loader), loss_d.mean().item(), loss_g.mean().item(), loss_hinge.mean().item(), loss_adv.mean().item(), acc/n) , end="\r")
+    return acc/n
+
+
+def train_semitargeted(G, D, f, thres, criterion_adv, criterion_gan, alpha, beta, train_loader, optimizer_G, optimizer_D, epoch, epochs, device, num_steps=3, verbose=True):
+    n = 0
+    acc = 0 # attack success rate
+    num_steps = num_steps
+    #target_pair = {'0':'5','1':'8','2':'8','3':'5','4':'9','5':'3','6':'2','7':'9','8':'5','9':'4'} # source-target pair dictionary
+    target_pair = {0:5,1:8,2:8,3:5,4:9,5:3,6:2,7:9,8:5,9:4} # source-target pair dictionary
+
+    G.train()
+    D.train()
+    for i, (img, label) in enumerate(train_loader): 
+        valid = Variable(torch.FloatTensor(img.size(0), 1).fill_(1.0).to(device), requires_grad=False)
+        fake = Variable(torch.FloatTensor(img.size(0), 1).fill_(0.0).to(device), requires_grad=False)
+
+        img_real = Variable(img.to(device))
+
+        optimizer_G.zero_grad()
+
+        pert = torch.clamp(G(img_real), -thres, thres) # clip to [-0.3,0.3]
+        img_fake = pert + img_real
+        img_fake = img_fake.clamp(min=0, max=1) # clip to [-1,1]
+
+        y_pred = f(img_fake)
+        #print(type(y_pred))
+        
+        # determine the corresponding target label
+        y_target=[]
+        #print(label)
+        for x in label.numpy():
+            #target_pair.get(x)
+             y_target.append(target_pair.get(x))
+        y_target = torch.LongTensor(y_target).to(device)
+        #print(y_target)
+        
+        # Train the Generator
+        # adversarial loss
+        #y_target = Variable(torch.ones_like(label).fill_(target).to(device))
+        loss_adv = criterion_adv(y_pred, y_target)
+        acc += torch.sum(torch.max(y_pred, 1)[1] == y_target).item()
 
         # GAN Generator loss
         loss_gan = criterion_gan(D(img_fake), valid)

ad1edb3dfb1f95d99cf1649be9b1be9524f9c3b4
Weijia <zhangweijia101@gmail.com>
2019-05-09 18:32:00 +0100

    added interclass distance and improved eval

--- advgan.py
+++ advgan.py
@@ -2,12 +2,15 @@ import torch
 import torch.nn.functional as F
 import target_models
 from generators import Generator_MNIST as Generator
+from test_function import test
+from prepare_dataset import load_dataset
+from train_function import train
+from test_function import test
 
 import cv2
 import numpy as np
 import os
 import argparse
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='AdvGAN for MNIST')
@@ -15,17 +18,25 @@ if __name__ == '__main__':
     parser.add_argument('--target', type=int, required=False, help='Target label')
     parser.add_argument('--bound', type=float, default=0.3, choices=[0.2, 0.3], required=False, help='Perturbation bound (0.2 or 0.3)')
     parser.add_argument('--img', type=str, default='images/0.jpg', required=False, help='Image to perturb')
+    parser.add_argument('--gpu', action='store_true', default=True, help='Use GPU?')
 
     args = parser.parse_args()
     model_name = args.model
     target = args.target
     thres = args.bound
     img_path = args.img
+    gpu = args.gpu   
+    
+    # alternatively set parameters here
+    thres = 0.3
+    model = 'Model_C'
+    digit = 2
+    target = 9
+    img_path = 'images/%d.jpg'%(digit)
 
     is_targeted = False
     if target in range(0, 10):
         is_targeted = True
-
 
     # load target model
     f = getattr(target_models, model_name)(1, 10)
@@ -56,16 +67,29 @@ if __name__ == '__main__':
     perturbation = pert.data.squeeze().numpy() # convert tensor to numpy
 
     # prediction before and after attack
-    prob_before, y_before = torch.max(F.softmax(f(x), 1), 1)
-    prob_after, y_after = torch.max(F.softmax(f(x_adv), 1), 1)
+    #prob_before, y_before = torch.max(F.softmax(f(x), 1), 1) # original implementation when softmax is not included in the model
+    #prob_after, y_after = torch.max(F.softmax(f(x_adv), 1), 1) 
+    prob_before, y_before = torch.max(f(x), 1)
+    prob_after, y_after = torch.max(f(x_adv), 1)
 
     print('Prediction before attack: %d [Prob: %0.4f]'%(y_before.item(), prob_before.item()))
     print('After attack: %d [Prob: %0.4f]'%(y_after.item(), prob_after.item()))
+    
+    # compute test attack success rate
+#    device = 'cuda' if gpu else 'cpu'
+#    if gpu:
+#        G.cuda()
+#        f.cuda()
+#    batch_size=128
+#    train_data, test_data, in_channels, num_classes = load_dataset('mnist')
+#    test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=4)
+#    acc_test, len_testloader = test(G, f, target, is_targeted, thres, test_loader, 1, 1, device, verbose=True)
+#    print('Test Acc: %.5f'%(acc_test))
 
     while True:
         cv2.imshow('Adversarial Image', adversarial_img)
-        cv2.imshow('Image', orig)
-        cv2.imshow('Perturbation', perturbation)
+#        cv2.imshow('Perturbation', perturbation)
+#        cv2.imshow('Image', orig)
 
         key = cv2.waitKey(10) & 0xFF
         if key == 27: # if ESC is pressed
@@ -75,9 +99,9 @@ if __name__ == '__main__':
             adversarial_img = adversarial_img*255 # restore to [0,255]
             adversarial_img = adversarial_img.astype(np.uint8) # set data type
             if is_targeted == True:
-                cv2.imwrite('targeted_%d_%d.png'%(target, y_after.item()), adversarial_img)
+                cv2.imwrite('targeted_%d_%d_%d.png'%(digit, target, y_after.item()), adversarial_img)
             if is_targeted == False:
-                cv2.imwrite('untargeted_%d.png'%(y_after.item()), adversarial_img)
+                cv2.imwrite('untargeted_%d_%d.png'%(digit, y_after.item()), adversarial_img)
             break
         
 cv2.destroyAllWindows()

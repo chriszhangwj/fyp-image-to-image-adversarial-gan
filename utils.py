f44c4a4a8f665a02f6dfa318811a929154192a42
Weijia <zhangweijia101@gmail.com>
2019-05-25 19:13:26 +0100

    add training evolution grid plot

--- utils.py
+++ utils.py
@@ -148,6 +148,27 @@ def tile_results():
         arr[a*28: (a+1)*28, b*28: (b+1)*28] = img
 
     import pdb; pdb.set_trace()
+    
+def tile_evolution():
+    arr = np.zeros((28*10, 28*11), dtype=np.uint8)
+    for i in range(10):
+        path = 'images/train_evolution/%d/'%(i)
+        images = os.listdir(path)
+        images.sort()
+        for j, image in enumerate(images):
+            img_path = os.path.join(path, image)
+            img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+
+            a = int(image.split('_')[0]) # split string and obtain key word
+            b = int(image.split('_')[2].split('.')[0])
+
+            arr[a*28: (a+1)*28, b*28: (b+1)*28] = img
+            
+    plt.figure(figsize=(7,7))
+    plt.imshow(arr, cmap = 'gray')
+    plt.axis('off')
+    #plt.tick_params(axis='both', which='both',bottom=False,top=False,labelbottom=False)
+    plt.show()    
 
 def perlin(size, period, octave, freq_sine, lacunarity = 2): # Perlin noise with sine color map
     
